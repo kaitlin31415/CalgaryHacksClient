3724be6649d1bdcfd28f867c94b6510bf0ef9317
RoxanneDuchesne <44510112+RoxanneDuchesne@users.noreply.github.com>
2020-02-15 16:43:32 -0500

    Smol Fix

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 
 class FoodItem extends Component {
-    constructor(props) {
-        super();
-        this.state =  {
-            name: props.name,
-            price: props.price,
-            restauant: props.restauant
-
-        };
-    }
+  constructor(props) {
+    super();
+    this.state = {
+      name: props.name,
+      price: props.price,
+      restaurant: props.restaurant,
+      location: props.location,
+      calories: props.calories,
+      listOfIDs: props.listOfIDs
+    };
+  }
+  
   render() {
+    /*
+    const l =  () => [1,2,3,4].map(function (item) {
+      return <li> {item} </li>;
+      
+    });
+    */
+   //const numbers = [1, 2, 3, 4, 5];
+   const listNums = this.state.listOfIDs.map((number) => 
+    <li>{number}</li>
+   );
+  
     return (
       <div>
-       This is a Food Item: {this.props.name}
+        <h1>Food Name: {this.state.name}</h1>
+        <h1>Restaurant: {this.state.restaurant}</h1>
+       
+        <ul>{listNums}</ul>
+
       </div>
-    )
+    );
+
   }
 }
 
