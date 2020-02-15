0c78a7c2e27012cebfa8c5c139c7c345d545db3c
kaitlin.decf <kaitlin.decf@gmail.com>
2020-02-15 15:21:55 -0700

    Components

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class SearchBox extends Component {
+    constructor(props) {
+        super();
+        this.state =  {
+            name: "Eunice"
+
+        };
+    }
   render() {
     return (
       <div>
-       This is a searchbox.
+       This is a searchbox. For {this.state.name}
       </div>
     )
   }
