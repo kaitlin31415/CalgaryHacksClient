c830e9dbb02c3f3ff7f4a069c4b412bc1a028933
kaitlin.decf <kaitlin.decf@gmail.com>
2020-02-15 15:17:28 -0700

    init

---
+++ src/components/SearchBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBox extends Component {
+  render() {
+    return (
+      <div>
+       This is a searchbox.
+      </div>
+    )
+  }
+}
+
+export default SearchBox;
---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SearchBox from './SearchBox';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+       tnjkafnkla.dnfk;ldsanfklaks
+       <SearchBox></SearchBox>
+      </div>
+    )
+  }
+}
+
+export default HomePage;
