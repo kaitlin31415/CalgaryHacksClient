57695664966325886da2368ff4952c6273729662
Eunice-Chan <echan298@mtroyal.ca>
2020-02-15 16:17:30 -0700

    dropdown woo

--- src/components/SearchCriteria.js
+++ src/components/SearchCriteria.js
@@ -13,6 +13,19 @@ class SearchCriteria extends Component {
     return (
       <div id='SearchCriteria'>
        This is a Search Criteria For the Category {this.state.category}
+       <label> Category
+          <select name="category" multiple>
+            <option value="" selected disabled> -Select Category- </option>
+            <option value="1"> 1 </option>
+            <option value="2"> 2 </option>
+            <option value="3"> 3 </option>
+            <option value="4"> 4 </option>
+            <option value="5"> 5 </option>
+            <option value="6"> 6 </option>
+            <option value="7"> 7 </option>
+            <option value="8"> 8 </option>
+          </select>
+      </label>
       </div>
     )
   }
