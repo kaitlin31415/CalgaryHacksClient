c63fb6872cefa390bb7ccbb884367c104ebf82d9
kaitlin.decf <kaitlin.decf@gmail.com>
2020-02-15 22:25:41 -0700

    button

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import FoodItem from './FoodItem';
+import FunButton from './FunButton';
 var tempFlavor = ["iconSweet", "iconSavory", "iconSour"];
 var tempDiet = ["Vegan", "Vegitarien", "Celic"];
 class HomePage extends Component {
@@ -9,6 +10,8 @@ class HomePage extends Component {
     return (
       <div>
        FEEDME I WANNA DIE
+       <FunButton />
+
        <SearchBox></SearchBox>
        <FoodItem name="Burger" price={5.25} restaurant="A" location="MacHall" calories={100}  listOfFlavors={tempFlavor} listOfDiets={tempDiet} />
       </div>
