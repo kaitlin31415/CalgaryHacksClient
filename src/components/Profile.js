3c026bc85fed59f334d18767a48c6d7bc71bafdb
Eunice-Chan <echan298@mtroyal.ca>
2020-02-16 11:20:07 -0700

    pretty

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,7 @@ var dinoArtsIcon = require('../images/dinoArts.png');
 var dinoScienceIcon = require('../images/dinoScience.png');
 var dinoNurseIcon = require('../images/dinoNurse.png');
 var dinoDefault = require("../images/dinoFucked.png");
+var burgerImg = require('../images/burger.jpg');
 
 class Profile extends Component {
   constructor(props) {
@@ -21,6 +22,9 @@ class Profile extends Component {
           <img id="profilepic" src={this.state.image} />
         </section>
         <h1>PROFILE NAME</h1>
+        <br></br>
+        <p>Favourites:</p>
+        <img id="dead" src={burgerImg}/>
 
       </div>
     )
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../SearchBox.css';
 import SearchCriteria from './SearchCriteria';
 import Slider from './Slider';
+var feedMeButton = require('../images/feedMeButton.png');
 
 class SearchBox extends Component {
     constructor(props) {
@@ -23,13 +24,11 @@ class SearchBox extends Component {
         <SearchCriteria category="Religious" options={["Halal","Kosher"]} />
         <h2>Cuisine:</h2> 
         <SearchCriteria category="Cuisine" options={["ALL", "American","Canadian","Chinese","Greek","Indian","Italian","Japanese", "Mexican", "Thai", "Vietnamese"]} />
-        <h2>Restaurant:</h2>
-        <SearchCriteria category="Restaurant" options={["ALL",2,3,4]} />
         <h2>Flavours:</h2>
         <SearchCriteria category="Flavours" options={["ALL", "Salty","Savoury","Sour","Spicy","Sweet"]} />
         <Slider title="Budget" min={0} max={100}></Slider>
         <Slider title="Calories" min={0} max={4000}></Slider>
-
+        <input type="image" src={feedMeButton} name="saveForm" class="btTxt submit" id="letsgo" />
       </div>
     )
   }
